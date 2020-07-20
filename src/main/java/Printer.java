470b6a370ebefbfc7dfa57af51e7205d284de29b
Ahmed Hamed <tunashark@gmail.com>
2020-07-20 15:21:08 +0100

    added print method

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -9,4 +9,13 @@ public class Printer {
         return this.numberOfSheetsLeft;
     }
 
+    public void print(int numberOfSheets, int numberOfCopies) {
+        if (this.numberOfSheetsLeft > 0) {
+            this.numberOfSheetsLeft -= numberOfSheets * numberOfCopies;
+        }
+        else {
+            System.out.println("The paper tray beckons you for more paper.");
+        }
+    }
+
 }
