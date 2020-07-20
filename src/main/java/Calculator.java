8b905087560a29c25854fc3c7022b497742d0ae8
Ahmed Hamed <tunashark@gmail.com>
2020-07-20 14:53:25 +0100

    add and subtract tests

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,10 @@
 public class Calculator {
+
+    public int add(int numberOne, int numberTwo) {
+        return numberOne + numberTwo;
+    }
+
+    public int subtract(int numberOne, int numberTwo) {
+        return numberOne - numberTwo;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,9 +1,24 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
-    @Before
-    public void canAdd() {
+    private Calculator calculator;
 
+    @Before
+    public void before() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void canAdd() {
+        assertEquals(3, calculator.add(1, 2));
+    }
+
+    @Test
+    public void canSubtract() {
+        assertEquals(1, calculator.subtract(2, 1));
     }
 
 }
