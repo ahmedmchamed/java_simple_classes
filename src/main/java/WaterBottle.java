d4fca677090b42dd649e24bc0047bd6a0d14b654
Ahmed Hamed <tunashark@gmail.com>
2020-07-20 15:01:53 +0100

    added methods to waterbottle class

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -1,2 +1,15 @@
 public class WaterBottle {
+    private int volume = 100;
+
+    public void WaterBottle(int volume) {
+        this.volume = volume;
+    }
+
+    public int drink(int drinkAmount) {
+        return this.volume -= drinkAmount;
+    }
+
+    public void emptyBottle() {
+        this.volume = 0;
+    }
 }
